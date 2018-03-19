5b38a648802021867f9b3d0384494fb0bff739b9
DQing <15829258737@163.com>
2018-03-19 09:36:24 +0800

    Update PracticeA.java

--- src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -6,7 +6,6 @@ import java.util.List;
 public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         List<String> expectedResult = new LinkedList<String>();
-        expectedResult.add("a");
         expectedResult.add("e");
         expectedResult.add("f");
         expectedResult.add("d");
